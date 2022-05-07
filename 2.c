5203333c357fc6b0bbf3a99b447eb8fa25af126e
SUDNYESH NEHARE <104644188+sudnyeshnehare@users.noreply.github.com>
2022-05-07 12:46:47 +0530

    Prolect 1

---
+++ 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int choice;
+    printf("\t\t\t\t\t\t\t\tBANK  MANAGEMENT  SYSTEM");
+    printf("\n\t\t\t1.Open New Account\n\t\t\t2.View Exixting Accounts in Bank\n\t\t\t3.Update information of existing account\n\t\t\t4.For transactions\n\t\t\t5.Removing existing account\n\t\t\t6.Check Account details\n\n\t\t\tEnter your choice :");
+    scanf("%d",&choice);
+    return 0;
+}
